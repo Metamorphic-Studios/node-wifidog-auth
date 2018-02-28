324831c24905c5e86cb532783d6046d4afe0d399
Ross Leitch <Ross@metamorphicsolutions.com>
2018-03-01 12:10:04 +1300

    Removed old landing

--- landing/src/components/google-button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import SocialLogin from 'react-social-login'
-
-const Button = ({ children, triggerLogin, ...props }) => (
-   <div className='wifi-login-gplus' onClick={triggerLogin}/>
-)
-
-export default SocialLogin(Button)
